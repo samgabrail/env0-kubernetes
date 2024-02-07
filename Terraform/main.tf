c7783e12b00f059dcb1c25d4efec136b38b5483b
Sam Gabrail <sam.gabrail@tekanaid.com>
2024-02-07 12:27:02 -0500

    bring name back

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ module "eks" {
 
   region               = "us-east-1"
   cluster_version      = "1.27"
-  cluster_name         = "env0_eks_cluster_monitoring2"
+  cluster_name         = "env0_eks_cluster_monitoring"
   instance_types       = ["t2.small"]
   vpc_cidr             = "10.0.0.0/16"
   cluster_min_size     = 1
